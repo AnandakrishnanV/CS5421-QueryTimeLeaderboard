9f423e6c0df32b9bb02db07b40966c2c6d2a71e4
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2022-04-10 18:55:04 +0800

    name correction

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="text-container">
         <h3  className="text-center">Implementation of a leaderboard Web service for the submission and automatic evaluation (according to performance or results) of SQL queries with PostgreSQL</h3>
         <h4 className="text-center">CS5421 Project</h4>
-        <h4 className="text-center">Anandakrishnan V, Ouyang Danwen, Song Qiaozhi, Yong Qin </h4>
+        <h4 className="text-center">Anandakrishnan V, Danwen Ouyang, Song Qiaozhi, Yong Qin </h4>
 
     </div>   
     </div>
